3e93a338a66fd851c60453dc44ba3d24725840f6
Andreas Gunnahr <Andreas.Gunnahr@hotmail.com>
2020-09-16 00:27:57 +0200

    add menu page and menuItem component

---
+++ client/src/components/MenuItem/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="menu__item">
+    <button class="add__btn">
+      <img src="@/assets/add.svg" />
+    </button>
+    <div class="item__container">
+      <div class="item__wrapper">
+        <h1 class="item__title">{{title}}</h1>
+        <span class="item__description">{{description}}</span>
+      </div>
+      <p class="item__price">{{price}} kr</p>
+    </div>
+  </div>
+</template>
+<script>
+import "./index.scss";
+export default {
+  name: "Menu Item",
+  props: {
+    title: String,
+    description: String,
+    price: String
+  }
+};
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,12 +3,18 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Landing from "../views/Landing.vue";
 import Order from "../views/Order.vue";
 import About from "../views/About.vue";
+import Menu from "../views/Menu.vue";
 
 const routes = [
   {
     path: "/",
     name: "Landing",
     component: Landing,
+  },
+  {
+    path: "/menu",
+    name: "Menu",
+    component: Menu,
   },
   {
     path: "/about",
